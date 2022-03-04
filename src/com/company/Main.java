f3970bc267c508b2e9372665ae2fe60414175ab0
Yana Kulagina <yana_post_letters@mail.ru>
2022-03-04 20:07:07 +0300

    third version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,5 +17,16 @@ public class Main {
         System.out.println(i);
         i = i + 1;
         System.out.println(i);
+
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("sum = " + c);
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+
+        String hello = "Hello";
+        String world = "World";
+        System.out.println(hello + " " + world + "!");
     }
 }
