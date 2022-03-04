1c196645809981abe49de92cfeb058e749893130
Yana Kulagina <yana_post_letters@mail.ru>
2022-03-04 19:31:41 +0300

    second version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
+
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+        System.out.println("Hello World!");
+
+        int i = 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
     }
 }
